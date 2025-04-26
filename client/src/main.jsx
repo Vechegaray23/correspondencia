11fc281e5cdc6537dcb1d0663bfad1ef17420bf5
Vicente Echegaray <viechegaray@alumnos.uai.cl>
2025-04-25 20:57:39 -0400

    fix(ui): añade bootstrap JS para que navbar colapse en móvil

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
