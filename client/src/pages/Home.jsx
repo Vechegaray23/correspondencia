ebd8f264470a9767600ed2074174ec0275e3d992
Vicente Echegaray <viechegaray@alumnos.uai.cl>
2025-04-28 14:06:57 -0400

    Cambios menores en frontend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>Bienvenido</h1>
-      <p>Pantalla de bienvenida del MVP.</p>
+      <p>Landing Page.</p>
     </div>
   );
 }
