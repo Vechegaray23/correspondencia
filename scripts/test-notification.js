ad0543d4a5dd0fdb066447274a7bafdc7e0971a5
Vicente Echegaray <viechegaray@alumnos.uai.cl>
2025-05-25 20:35:51 -0400

    fix: test-notification en JS, importa NotificationService.js

---
+++ scripts/test-notification.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { nuevoPaquete } from '../src/infra/notifications/NotificationService.js';
+
+(async () => {
+  try {
+    await nuevoPaquete({ id: 999, destinatario: 'Prueba CLI' }, process.env.SMTP_USER);
+    console.log('✔ Correo + SMS enviados');
+    process.exit(0);
+  } catch (err) {
+    console.error('✖ Falló el envío:', err);
+    process.exit(1);
+  }
+})();
